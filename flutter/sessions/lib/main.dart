e536198a978312fed9e214e84f09b7d267c2eead
Mohamed Hassan <mohamedhassankamel9@gmail.com>
2026-04-04 17:37:48 +0200

    Add LayoutDemoScreen with layout structure and background image

--- flutter/sessions/lib/main.dart
+++ flutter/sessions/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sessions/1_dashboard_screen/screens/dashboard/dashboard_screen.dart';
+import 'package:sessions/2_layout_demo/layout_demo_screen.dart';
 
 void main() {
-  runApp(const DashboardScreen());
+  runApp(const LayoutDemoScreen());
 }
