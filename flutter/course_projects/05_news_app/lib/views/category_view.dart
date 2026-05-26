28c5043ffa9a388732ed2e955aaa84f23f007b0e
Mohamed Hassan <mohamedhassankamel9@gmail.com>
2026-05-26 10:32:02 +0300

    feat: implement category navigation and asynchronous news list building using FutureBuilder

---
+++ flutter/course_projects/05_news_app/lib/views/category_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/news_list_view_builder.dart';
+
+class CategoryView extends StatelessWidget {
+  const CategoryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('News', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              'Cloud',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.orange,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: CustomScrollView(
+          physics: BouncingScrollPhysics(),
+          slivers: [NewsListViewBuilder()],
+        ),
+      ),
+    );
+  }
+}
--- flutter/course_projects/05_news_app/lib/widgets/category_card.dart
+++ flutter/course_projects/05_news_app/lib/widgets/category_card.dart
@@ -1,27 +1,32 @@
 import 'package:_05_news_app/models/categories_model.dart';
 import 'package:flutter/material.dart';
 
+import '../views/category_view.dart';
+
 class CategoryCard extends StatelessWidget {
   const CategoryCard({super.key, required this.category});
   final CategoryModel category;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: Image.asset(category.image).image,
-          fit: BoxFit.fill,
+    return GestureDetector(
+      onTap: () => CategoryView(),
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: Image.asset(category.image).image,
+            fit: BoxFit.fill,
+          ),
+          borderRadius: BorderRadius.circular(12),
         ),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      width: 250,
-      child: Center(
-        child: Text(
-          category.category,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+        width: 250,
+        child: Center(
+          child: Text(
+            category.category,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
